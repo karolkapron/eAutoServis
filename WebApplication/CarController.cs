caa6e23c278739e8d9bf3bddd40371c75420f1ec
Kuba Marchut <kuba.marchut@gmail.com>
2023-03-12 11:43:20 +0100

    Adding web API to car microservice

---
+++ WebApplication/CarController.cs
@@ -0,0 +1,39 @@
+﻿namespace WebApplication
+{
+    using BAJK.EAutoSerwis.solution.xLib;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CarController : ControllerBase, ICarLot
+    {
+        private readonly ILogger<CarController> logger;
+        private readonly CarLot carLot;
+
+        public CarController(ILogger<CarController> logger)
+        {
+            this.logger = logger;
+            this.carLot = new CarLot();
+        }
+
+        [HttpGet]
+        [Route("GetExpiredCheckUp")]
+        public Car[] FindExpiredCheckUp()
+        {
+            return this.carLot.FindExpiredCheckUp();
+        }
+        [HttpGet]
+        [Route("GetSameBrands")]
+        public Car[] FindAllTheSameBrands(string name)
+        {
+            return this.carLot.FindAllTheSameBrands(name);
+        }
+    }
+}
