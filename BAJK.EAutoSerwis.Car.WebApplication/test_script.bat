ba550b0255cb1bb6a27176942f46d97b7de80ce5
Kuba Marchut <kuba.marchut@gmail.com>
2023-03-15 10:55:57 +0100

    correcting port numbers in test script

--- BAJK.EAutoSerwis.Car.WebApplication/test_script.bat
+++ BAJK.EAutoSerwis.Car.WebApplication/test_script.bat
@@ -1,4 +1,6 @@
-curl -X "GET" "https://localhost:44328/Car/GetSameBrands?name=Opel" -H "accept: text/plain"
+curl -X "GET" "https://localhost:5001/Car/GetSameBrands?name=Opel" -H "accept: text/plain"
+
+curl -X "GET" "http://localhost:5000/Car/GetSameBrands?name=Opel" -H "accept: text/plain"
 
 pause
 	
